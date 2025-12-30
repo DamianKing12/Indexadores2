d41d0feca7dd617dc98644b81fae7fdbd05fbb51
PC <damian.a.king.r.12@gmail.com>
2025-12-29 20:46:41 -0400

    Prueba 3

--- SeriesKaoProvider/src/main/kotlin/com/DamianKing12/SeriesKaoProvider.kt
+++ SeriesKaoProvider/src/main/kotlin/com/DamianKing12/SeriesKaoProvider.kt
@@ -3,17 +3,6 @@ package com.DamianKing12
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
-
-@CloudstreamPlugin
-class SeriesKaoPlugin: Plugin() {
-    override fun load(context: Context) {
-        // Registramos la clase del proveedor
-        registerMainAPI(SeriesKaoProvider())
-    }
-}
 
 class SeriesKaoProvider : MainAPI() {
     override var name = "SeriesKao"

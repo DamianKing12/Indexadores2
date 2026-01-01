4ee32a41ad6b84f76c8661c9cc4e97249d0e482b
PC <damian.a.king.r.12@gmail.com>
2026-01-01 09:31:37 -0400

    Prueba 32

--- SeriesKaoProvider/src/main/kotlin/com/DamianKing12/SeriesKaoProvider.kt
+++ SeriesKaoProvider/src/main/kotlin/com/DamianKing12/SeriesKaoProvider.kt
@@ -3,55 +3,30 @@ package com.DamianKing12
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
-
-@CloudstreamPlugin
-class SeriesKaoPlugin : BasePlugin() {
-    override fun load() {
-        // Registramos el proveedor
-        registerMainAPI(SeriesKaoProvider())
-    }
-}
 
 class SeriesKaoProvider : MainAPI() {
-    // CAMBIO CLAVE: El name debe ser igual al internalName del JSON
-    override var name = "SeriesKaoProvider" 
+    override var name = "SeriesKao"
     override var mainUrl = "https://serieskao.top"
     override var supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
     override var lang = "es"
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val url = "$mainUrl/?s=$query"
-        val response = app.get(url)
-        val document = response.document
-        
+        val url = "$mainUrl/?s=${query.replace(" ", "+")}"
+        val document = app.get(url).document
         return document.select("div.result-item").mapNotNull {
             val title = it.selectFirst("div.title a")?.text() ?: return@mapNotNull null
             val href = it.selectFirst("div.title a")?.attr("href") ?: ""
             val poster = it.selectFirst("img")?.attr("src")
-
-            newMovieSearchResponse(title, href, TvType.Movie) {
-                this.posterUrl = poster
-            }
+            newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = poster }
         }
     }
 
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile)->Unit, callback: (ExtractorLink)->Unit): Boolean {
         val doc = app.get(data).document
         doc.select("iframe").amap {
             var iframeUrl = it.attr("src")
-            if (iframeUrl.startsWith("//")) {
-                iframeUrl = "https:$iframeUrl"
-            }
-            if (iframeUrl.isNotEmpty()) {
-                loadExtractor(iframeUrl, data, subtitleCallback, callback)
-            }
+            if (iframeUrl.startsWith("//")) iframeUrl = "https:$iframeUrl"
+            loadExtractor(iframeUrl, data, subtitleCallback, callback)
         }
         return true
     }
